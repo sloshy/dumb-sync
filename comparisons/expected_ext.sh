5fc3f0a51a04d0a321303e9fb761313b210419dc
Ryan Peters <me@rpeters.dev>
2023-03-29 15:43:00 -0500

    Check exact file name after grep search

--- comparisons/expected_ext.sh
+++ comparisons/expected_ext.sh
@@ -14,23 +14,36 @@ FILE_LOCAL_BASE=$(basename -- "$FILE_LOCAL" ".$LOCAL_EXT")
 
 # (Date) (Time) (Filename)
 FILE_LINE_REGEX="\b([0-9]{4}/[0-9]{2}/[0-9]{2}) ([0-9]{2}:[0-9]{2}:[0-9]{2}) (.*)\b"
+EXPECTED_FILE="$FILE_LOCAL_BASE.$REMOTE_EXT"
 
 set +e
-FILE_LINE=$(grep -F "$FILE_LOCAL_BASE.$REMOTE_EXT" "$FILE_LIST")
+GREP_OUT=$(grep -F "$FILE_LOCAL_BASE.$REMOTE_EXT" "$FILE_LIST")
 if [[ "$?" -eq 0 ]]; then
-  if [[ "$FILE_LINE" =~ $FILE_LINE_REGEX ]]; then
-    FILE_DATE=${BASH_REMATCH[1]}
-    FILE_TIME=${BASH_REMATCH[2]}
-    FILE_NAME=${BASH_REMATCH[3]}
-    FILE_SECS=$(date -d "$FILE_DATE $FILE_TIME" +%s)
-    FILE_SECS_OFFSET=$((FILE_SECS + SYNC_OFFSET_SECS))
-    if [[ "$LAST_SYNC_TIME_SECS" -le "$FILE_SECS_OFFSET" ]]; then
-      echo "updated"
+  while read -r FILE_LINE; do
+    if [[ "$FILE_LINE" =~ $FILE_LINE_REGEX ]]; then
+      FILE_DATE=${BASH_REMATCH[1]}
+      FILE_TIME=${BASH_REMATCH[2]}
+      FILE_NAME=${BASH_REMATCH[3]}
+
+      [[ "$FILE_NAME" == "$EXPECTED_FILE" ]] || continue
+
+      FILE_SECS=$(date -d "$FILE_DATE $FILE_TIME" +%s)
+      FILE_SECS_OFFSET=$((FILE_SECS + SYNC_OFFSET_SECS))
+      if [[ "$LAST_SYNC_TIME_SECS" -le "$FILE_SECS_OFFSET" ]]; then
+        echo "updated"
+      else
+        echo "current $FILE_NAME"
+      fi
     else
-      echo "current $FILE_NAME"
+      exit 1
     fi
+  done < <(echo "$GREP_OUT")
+  # See if the file is just not transformed yet
+  BASE_WITHOUT_REMOTE=${FILE_LOCAL_BASE%"$REMOTE_EXT"}
+  if grep -Fq "$BASE_WITHOUT_REMOTE" "$FILE_LIST"; then
+    echo "transform"
   else
-    exit 1
+    echo "missing"
   fi
 else
   # See if the file is just not transformed yet
